941d8d9a882588f62cb53edee87461920d140eef
Kevin Py <kevin.py_ssii@i-bp.fr>
2019-09-24 16:55:17 +0200

    ADD TU

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,25 @@
 import { create, get, set, remove } from "../src/index";
+const index = require('../src/index');
+
+const key = 'test';
+const value = [{name: 'John'}];
 
 test('create', () => {
-  const key = 'test';
-  const initValue = [];
   expect(create(key)).toEqual([]);
 });
 
-test('create 2', () => {
-  const key = 'test';
-  const value = [{name: 'John'}];
-  create(key);
-  set(key, value)
-  expect(get(key)).toEqual([{name: 'John'}]);
+test('set', () => {
+  const spySet = jest.spyOn(index, 'set');
+  set(key, value);
+  expect(spySet).toHaveBeenCalled();
+});
+
+test('get', () => {
+  expect(get(key)).toEqual(value);
+});
+
+test('remove', () => {
+  const spyRemove = jest.spyOn(index, 'remove');
+  remove(key);
+  expect(spyRemove).toHaveBeenCalled();
 });
